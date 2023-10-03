0d84fe69d5518b2b768d559ed6e9c9b83573fba4
tanmayhinge <hingetanmay@gmail.com>
2023-10-02 20:27:54 -0700

    optimal cut

--- rod-cutting.cpp
+++ rod-cutting.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-template<typename T>
-void printMatrix(T mat) {
-    cout<<"\n Printing Matrix : \n";
-    for(int i=0 ; i<=11-1 ; i++) {
-        for(int j=0 ; j<=11-1 ; j++)
-            cout<< *(*(mat+i)+j)<<" ";
-        cout<<endl;
+template <typename T>
+void printMatrix(T mat)
+{
+    cout << "\n Printing Matrix : \n";
+    for (int i = 0; i <= 11 - 1; i++)
+    {
+        for (int j = 0; j <= 11 - 1; j++)
+            cout << *(*(mat + i) + j) << " ";
+        cout << endl;
     }
-    cout<<endl;
+    cout << endl;
 }
 
+void printOptimalCut(vector<vector<int> >& sizes, vector<vector<int> >& decompositions, int n) {
+    cout << "Optimal sizes and decompositions:\n";
+    for (int i = 1; i <= n; ++i) {
+        cout << "Rod length " << i << ": ";
+        int size = sizes[n][i];
+        vector<int> cut;
+        while (size > 0) {
+            cut.push_back(decompositions[n][size]);
+            size -= decompositions[n][size];
+        }
+        cout << "Optimal Size: " << sizes[n][i] << ", Decomposition: ";
+        for (int j = cut.size() - 1; j >= 0; --j) {
+            cout << cut[j] << " ";
+        }
+        cout << endl;
+    }
+}
 int rod_cutting(int arr[], int n)
 {
-    int matrix[11][11]={0};
+    int matrix[11][11] = {0};
+    vector<vector<int> > sizes(11, vector<int>(11, 0));
+    vector<vector<int> > decompositions(11, vector<int>(11, 0));
+
     for (int i = 0; i <= n; i++)
     {
         for (int j = 0; j <= n; j++)
@@ -29,32 +51,49 @@ int rod_cutting(int arr[], int n)
                 if (i == 1)
                 {
                     matrix[i][j] = j * arr[i - 1];
+                    sizes[i][j] = 1;
+                    decompositions[i][j] = 1;
                 }
                 else
                 {
                     if (i > j)
                     {
                         matrix[i][j] = matrix[i - 1][j];
+                        sizes[i][j] = sizes[i - 1][j];
+                        decompositions[i][j] = decompositions[i - 1][j];
                     }
                     else
                     {
-                        matrix[i][j] = max(arr[i - 1] + matrix[i][j - i], matrix[i - 1][j]);
+                        int option1 = arr[i - 1] + matrix[i][j - i];
+                        int option2 = matrix[i - 1][j];
+                        if (option1 > option2)
+                        {
+                            matrix[i][j] = option1;
+                            sizes[i][j] = i;
+                            decompositions[i][j] = i;
+                        }
+                        else
+                        {
+                            matrix[i][j] = option2;
+                            sizes[i][j] = sizes[i - 1][j];
+                            decompositions[i][j] = decompositions[i - 1][j];
+                        }
                     }
                 }
             }
         }
-            printMatrix(matrix);
+        printMatrix(matrix);
     }
+
+    printOptimalCut(sizes, decompositions, n);
+
     return matrix[n][n];
 }
 
 int main()
 {
     int arr[] = {0, 5, 10, 0, 12, 18, 0, 22, 25, 30};
-
     int n = sizeof(arr) / sizeof(arr[0]);
-
     cout << rod_cutting(arr, n);
-
     return 0;
 }
